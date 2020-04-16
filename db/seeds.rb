a46fce957001919df35313f161a1d2dfc700f3cd
Juliana Ariffin <juliana@fastmail.com>
2020-04-16 12:42:46 +0200

    migrated and seeded with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+
+10.times do
+  flat = Flat.create(
+    name: Faker::Movies::LordOfTheRings.location,
+    address: Faker::Address.street_address,
+    description: Faker::Books::Lovecraft.sentence,
+    price_per_night: [100..1000].sample,
+    number_of_guests: [1..5].sample)
+    puts "Created #{flat.name}, #{flat.description}, #{flat.price_per_night}"
+end
+
+puts "Finished!"
